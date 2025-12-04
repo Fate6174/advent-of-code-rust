824532956a9f30e7265fad3e95f189ed33edf01b
Henning Sauter <henning.sauter@outlook.com>
2025-12-04 20:19:58 +0100

    more elegant representation by using HashSet instead of ndarray (slower though)

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,63 +1,63 @@
-use ndarray::Array2;
+use num::complex::Complex;
+use std::collections::HashSet;
+
+const OFFSETS: [Complex<isize>; 8] = [
+    Complex::new(1, 0),
+    Complex::new(-1, 0),
+    Complex::new(0, 1),
+    Complex::new(0, -1),
+    Complex::new(1, 1),
+    Complex::new(1, -1),
+    Complex::new(-1, 1),
+    Complex::new(-1, -1),
+];
 
 advent_of_code::solution!(4);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let grid = parse_grid(input);
-    Some(find_removables(&grid).len() as u64)
+    let grid_set = parse_grid(input);
+    Some(find_removables(&grid_set).len() as u64)
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let mut grid = parse_grid(input);
+    let mut grid_set = parse_grid(input);
     let mut out = 0;
     loop {
-        let removables = find_removables(&grid);
+        let removables = find_removables(&grid_set);
         if removables.is_empty() {
             break;
         }
-        for (i, j) in &removables {
-            grid[[*i, *j]] = '.';
+        for coordinate in &removables {
+            grid_set.remove(coordinate);
         }
         out += removables.len();
     }
     Some(out as u64)
 }
 
-fn parse_grid(input: &str) -> Array2<char> {
-    let rows: Vec<&str> = input.lines().collect();
-    let m = rows.len();
-    let n = rows[0].len();
-    let flat: Vec<char> = rows.iter().flat_map(|row| row.chars()).collect();
-    Array2::from_shape_vec((m, n), flat).expect("grid is not rectangular")
+fn parse_grid(input: &str) -> HashSet<Complex<isize>> {
+    let mut out = HashSet::new();
+    for (row_idx, row) in input.lines().enumerate() {
+        for (col_idx, char) in row.chars().enumerate() {
+            if char == '@' {
+                out.insert(Complex::new(row_idx as isize, col_idx as isize));
+            }
+        }
+    }
+    out
 }
 
-fn find_removables(grid: &Array2<char>) -> Vec<(usize, usize)> {
-    let (m, n) = grid.dim();
+fn find_removables(grid_set: &HashSet<Complex<isize>>) -> Vec<Complex<isize>> {
     let mut out = vec![];
-    for row_idx in 0..m {
-        for col_idx in 0..n {
-            if grid[[row_idx, col_idx]] == '@' {
-                // count adjacent paper rolls (@)
-                let mut adjacent_rolls = 0;
-                for row_idx_temp in [row_idx.wrapping_sub(1), row_idx, row_idx + 1] {
-                    if row_idx_temp == usize::MAX || row_idx_temp == m {
-                        continue;
-                    }
-                    for col_idx_temp in [col_idx.wrapping_sub(1), col_idx, col_idx + 1] {
-                        if col_idx_temp == usize::MAX || col_idx_temp == n {
-                            continue;
-                        }
-                        if (row_idx_temp, col_idx_temp) != (row_idx, col_idx)
-                            && grid[[row_idx_temp, col_idx_temp]] == '@'
-                        {
-                            adjacent_rolls += 1
-                        }
-                    }
-                }
-                if adjacent_rolls < 4 {
-                    out.push((row_idx, col_idx));
-                }
+    for coordinate in grid_set {
+        let mut adjacent_rolls = 0;
+        for dz in OFFSETS {
+            if grid_set.contains(&(coordinate + dz)) {
+                adjacent_rolls += 1
             }
+        }
+        if adjacent_rolls < 4 {
+            out.push(*coordinate);
         }
     }
     out
