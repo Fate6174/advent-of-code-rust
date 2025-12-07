9892203e359a7b6f91a45acefd6f55bafa609929
Henning Sauter <henning.sauter@outlook.com>
2025-12-07 20:59:41 +0100

    minor simplification in day 06

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -31,10 +31,7 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    let parsed = input
-        .lines()
-        .map(|line| line.as_bytes())
-        .collect::<Vec<_>>();
+    let parsed = input.lines().map(str::as_bytes).collect::<Vec<_>>();
     let num_rows = parsed.len();
     let mut temp = Vec::new();
     let mut out = 0;
